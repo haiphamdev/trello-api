78f33cf089ee61f939a32484bd7fbb94b271db3b
Hai Pham <haipham.ndev@gmail.com>
2022-06-10 20:05:07 +0700

    Init project Node.JS

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+const hostname = "localhost";
+const port = 8088;
+
+app.get("/", (req, res) => {
+  res.end("<h1>Hello word!</h1>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Hello haipham, I'm running at ${hostname}:${port}/`);
+});
