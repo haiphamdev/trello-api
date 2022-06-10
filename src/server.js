12610cb8ca2bf83ed3231d70d636cacce2076d33
Hai Pham <haipham.ndev@gmail.com>
2022-06-10 21:58:31 +0700

    Config babeljs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { mapOrder } from "./utilities/sorts.js";
 
 const app = express();
 
@@ -6,7 +7,7 @@ const hostname = "localhost";
 const port = 8088;
 
 app.get("/", (req, res) => {
-  res.end("<h1>Hello word!</h1>");
+  res.end("<h1>Hello word !</h1>");
 });
 
 app.listen(port, hostname, () => {
