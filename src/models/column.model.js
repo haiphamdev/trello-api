cd0b3cabd96b1ba44ba0a6ea3736c33a5cf06c57
Hai Pham <haipham.ndev@gmail.com>
2022-06-11 14:56:30 +0700

    Design Database Schema & manage mongodb connection

---
+++ src/models/column.model.js
@@ -0,0 +1,47 @@
+import Joi from "joi";
+import { ObjectId } from "mongodb";
+import { getDB } from "../config/mongodb.js";
+
+// Define Column collection
+const columnCollectionName = "columns";
+const columnCollectionSchema = Joi.object({
+  boardId: Joi.string().required(),
+  title: Joi.string().required().min(3).max(20),
+  cardOrder: Joi.array().items(Joi.string()).default([]),
+  createdAt: Joi.date().timestamp().default(Date.now()),
+  updatedAt: Joi.date().timestamp().default(null),
+  _destroy: Joi.boolean().default(false),
+});
+
+const validateSchema = async (data) => {
+  return await columnCollectionSchema.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+
+const findOneById = async (id) => {
+  try {
+    const result = await getDB()
+      .collection(columnCollectionName)
+      .findOne({ _id: ObjectId(id) });
+    return result;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const createNew = async (data) => {
+  try {
+    const value = await validateSchema(data);
+    const result = await getDB()
+      .collection(columnCollectionName)
+      .insertOne(value);
+    // return result;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+    // throw new Error(error);
+  }
+};
+
+export const ColumnModel = { createNew, findOneById };
